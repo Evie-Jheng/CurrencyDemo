f953f9a4b9b15fcaa3fd45e4ababeaf69b6b0e2d
Evie <JhengYiJing@gmail.com>
2023-04-18 22:44:19 +0800

    init

---
+++ src/test/java/com/example/CurrencyDemo/CurrencyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CurrencyDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
